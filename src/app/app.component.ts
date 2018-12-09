5356aef79e30a35b54fc93f2d6d12717d6954e21
Gitea <gitea@fake.local>
2018-12-09 17:56:26 +0800

    update

---
+++ src/app/app.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+  public appPages = [
+    {
+      title: 'Home',
+      url: '/home',
+      icon: 'home'
+    },
+    {
+      title: 'Maps',
+      url: '/maps',
+      icon: 'map'
+    },
+    {
+      title: 'Shop List',
+      url: '/list',
+      icon: 'list'
+    },
+    {
+      title: 'Messages',
+      url: '/messages',
+      icon: 'chatboxes'
+    },
+    {
+      title: 'Register',
+      url: '/register',
+      icon: 'add'
+    },
+    {
+      title: 'Login',
+      url: '/login',
+      icon: 'log-in'
+    }
+  ];
+
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+}
