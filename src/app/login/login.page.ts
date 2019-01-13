5acc6d5c66eadcbf276ad92858e93b3b2659469a
jhonortes <jhonortes@yahoo.com>
2019-01-13 21:23:44 +0800

    update login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,63 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { AlertController } from '@ionic/angular';
-import { Firebase } from '@ionic-native/firebase/ngx';
+import { AlertController } from '@ionic/angular'; 
+import { Router, ActivatedRoute } from '@angular/router';
+import { AppComponent } from '../app.component'; 
+
 
 @Component({
   selector: 'app-list',
   templateUrl: 'login.page.html',
-  styleUrls: ['login.page.scss'],
-  providers: [
-    Firebase
-  ]
+  styleUrls: ['login.page.scss']
 })
-export class LoginPage implements OnInit {
-  private selectedItem: any;
-  private icons = [
-    'flask',
-    'wifi',
-    'beer',
-    'football',
-    'basketball',
-    'paper-plane',
-    'american-football',
-    'boat',
-    'bluetooth',
-    'build'
-  ];
-  public items: Array<{ title: string; note: string; icon: string }> = [];
-  constructor(private firebase: Firebase) {
-
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
+export class LoginPage implements OnInit {  
+  txtusername: "";
+  txtpassword: "";
+  constructor(public router: Router, public alertCtrl: AlertController,private util: AppComponent) {}
+  submitlogin(){
+    if( typeof(this.txtusername) != 'undefined' && typeof(this.txtpassword) != 'undefined' ){
+      this.util.login(this.txtusername,this.txtpassword);
+    } else {
+      this.util.alerts('Login','Username or password didnot same',['ok']);
     }
-
-    async function presentAlert() {
-      const alertController = document.querySelector('ion-alert-controller');
-      await alertController.componentOnReady();
-    
-      const alert = await alertController.create({
-        header: 'Alert',
-        subHeader: 'Subtitle',
-        message: 'This is an alert message.',
-        buttons: ['OK']
-      });
-      return await alert.present();
-    }
-
   }
-
   ngOnInit() {
+  } 
+  navigate() {
+    this.router.navigate(['/home']);
   }
-  // add back when alpha.4 is out
-  // navigate(item) {
-  //   this.router.navigate(['/list', JSON.stringify(item)]);
-  // }
-  dismiss(){
-    // this.viewCtrl.dismiss();
-  }
-
 }
