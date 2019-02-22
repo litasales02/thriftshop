63e78fb530036fa0d1c81418d1f5c6e3c61f14cf
jhonortes <jhonortes@yahoo.com>
2019-02-22 13:33:25 +0800

    update

--- src/app/registersellers/registersellers.page.ts
+++ src/app/registersellers/registersellers.page.ts
@@ -29,57 +29,59 @@ export class RegisterSellersPage implements OnInit {
   constructor(public router: Router, public alertCtrl: AlertController,private util: AppComponent) {}
   ngOnInit() {}
   submitdata(){
-    this.util.getuserlogbyname(this.sellerusername,(result)=>{
-      if(result){        
-        this.util.alerts("Add New","Username is already used",['Ok']);
-        return;
-      }else{
-         if (typeof(this.sellerstorename) != 'undefined' && 
-    typeof(this.selleraddress1) != 'undefined' && 
-    typeof(this.selleraddress2) != 'undefined' && 
-    typeof(this.sellerfirstname) != 'undefined'  && 
-    typeof(this.sellerlastname) != 'undefined'  && 
-    typeof(this.sellermiddlename) != 'undefined' && 
-    typeof(this.sellerusername) != 'undefined' && 
-    typeof(this.sellerpassword) != 'undefined' ){
-      if (typeof(this.sellerpassword) != 'undefined' && typeof(this.sellerrepassword) != 'undefined' && this.sellerrepassword == this.sellerpassword){
-        this.util.newdata({
-          'userdetails': { 
-            'address1': this.selleraddress1!=""?this.selleraddress1:'none', 
-            'address2': this.selleraddress2!=""?this.selleraddress2:'none', 
-            'firstname': this.sellerfirstname!=""?this.sellerfirstname:'none', 
-            'lastname': this.sellerlastname!=""?this.sellerlastname:'none', 
-            'middlename': this.sellermiddlename!=""?this.sellermiddlename:'none',
-            'cellnumber': this.cellnumber!=""?this.cellnumber:'none',
-            'telnumber': typeof(this.telnumber) != 'undefined'?this.telnumber:'none',
-            'email': typeof(this.emails) != 'undefined'?this.emails:'none',
-            "profileimg": typeof(this.iamgefile) != 'undefined'?this.iamgefile:'none'
-          },
-          'usertype': 'seller',
-          'username': this.sellerusername!=""?this.sellerusername:'none', 
-          'password': this.sellerpassword!=""?this.sellerpassword:'none',
-          'storename': this.sellerstorename!=""?this.sellerstorename:'none',
-          'requirements' : { 
-            'status': 0,
-            'govid': 'None',
-            'storeimg':'None'
-          },
-          'geodata': {
-            'status': 1,
-            'lat': '0.0',
-            'lng': '0.0'
-          }
-        });
-        this.navigate();
-      } else if (typeof(this.sellerpassword) != 'undefined' && typeof(this.sellerrepassword) && (this.sellerrepassword.length < 6 || this.sellerpassword.length < 6 )) {
-        this.util.alerts("Add New","User password must be minimum of 6 character",['Ok']);
-      }else {
-        this.util.alerts("Add New","User password did not equal",['Ok']);
-      }
-    } else {
-    }
-      }
+    var results = this.util.getuserlogbyname(this.sellerusername,(result)=>{
+      return result;
+      // if(result){        
+      //   this.util.alerts("Add New","Username is already used",['Ok']); 
+      // } else {
+      //    if (typeof(this.sellerstorename) != 'undefined' && 
+      //   typeof(this.selleraddress1) != 'undefined' && 
+      //   typeof(this.selleraddress2) != 'undefined' && 
+      //   typeof(this.sellerfirstname) != 'undefined'  && 
+      //   typeof(this.sellerlastname) != 'undefined'  && 
+      //   typeof(this.sellermiddlename) != 'undefined' && 
+      //   typeof(this.sellerusername) != 'undefined' && 
+      //   typeof(this.sellerpassword) != 'undefined' ){
+      //     if (typeof(this.sellerpassword) != 'undefined' && typeof(this.sellerrepassword) != 'undefined' && this.sellerrepassword == this.sellerpassword){
+      //       this.util.newdata({
+      //         'userdetails': { 
+      //           'address1': this.selleraddress1!=""?this.selleraddress1:'none', 
+      //           'address2': this.selleraddress2!=""?this.selleraddress2:'none', 
+      //           'firstname': this.sellerfirstname!=""?this.sellerfirstname:'none', 
+      //           'lastname': this.sellerlastname!=""?this.sellerlastname:'none', 
+      //           'middlename': this.sellermiddlename!=""?this.sellermiddlename:'none',
+      //           'cellnumber': this.cellnumber!=""?this.cellnumber:'none',
+      //           'telnumber': typeof(this.telnumber) != 'undefined'?this.telnumber:'none',
+      //           'email': typeof(this.emails) != 'undefined'?this.emails:'none',
+      //           "profileimg": typeof(this.iamgefile) != 'undefined'?this.iamgefile:'none'
+      //         },
+      //         'usertype': 'seller',
+      //         'username': this.sellerusername!=""?this.sellerusername:'none', 
+      //         'password': this.sellerpassword!=""?this.sellerpassword:'none',
+      //         'storename': this.sellerstorename!=""?this.sellerstorename:'none',
+      //         'requirements' : { 
+      //           'status': 0,
+      //           'govid': 'None',
+      //           'storeimg':'None'
+      //         },
+      //         'geodata': {
+      //           'status': 1,
+      //           'lat': '0.0',
+      //           'lng': '0.0'
+      //         }
+      //       });
+      //       this.navigate();
+      //     } else if (typeof(this.sellerpassword) != 'undefined' && typeof(this.sellerrepassword) && (this.sellerrepassword.length < 6 || this.sellerpassword.length < 6 )) {
+      //       this.util.alerts("Add New","User password must be minimum of 6 character",['Ok']);
+      //     }else {
+      //       this.util.alerts("Add New","User password did not equal",['Ok']);
+      //     }
+      //   } else {
+      //   }
+      // }
     });
+    
+    console.log(results);
    
   }
   navigate() {
